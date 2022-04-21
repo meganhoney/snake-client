5dfd6d137a6784676960f704799e3f2bcec29f9a
Megan Honey <mjthoney@gmail.com>
2022-04-21 01:05:55 +0000

    Add quit function with ctrl+c

--- play.js
+++ play.js
@@ -1,2 +1,22 @@
 // main file through which game is launched
-const client = require('./client');
+const {connect} = require('./client');
+
+const handleUserInput = function (input) {
+  if (input === '\u0003') {
+    process.exit();
+  };
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
+setupInput();
