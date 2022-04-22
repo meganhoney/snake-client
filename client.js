b56efa0710dfaf7dab761657e810c0ed4e438a86
Megan Honey <mjthoney@gmail.com>
2022-04-22 23:04:50 +0000

    Remove idle message, not part of main functionality, lint code

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require('net');
 const { IP, PORT } = require("./constants");
 // establishes server connection
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: IP,
     port: PORT,
@@ -11,34 +11,20 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  //print message once connected
+  // print message once connected and send name to server
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-  });
-
-  conn.on('connect', () => {
     conn.write('Name: MH');
   });
 
-  // setInterval(() => {
-  //   conn.write('Move: up');
-  // }, 50);
-  // conn.on('connect', () => {
-  //   conn.write('Move: up');
+  // after 5 sec of idling, kick user out of game
+  // conn.setTimeout(4000);
+  // conn.on('timeout', () => {
+  //   console.log('you ded cuz you idled');
+  //   process.exit();
   // });
-
-  conn.setTimeout(2000);
-  conn.on('timeout', () => {
-    console.log('you ded cuz you idled');
-  });
 
   return conn;
 };
 
 module.exports = {connect};
-
-// Consider what is happening in the code that you wrote so far:
-
-// You used Node's net library (specifically, the createConnection function) to create an object named conn in the code above.
-// The conn object that Node returned to you represents the connection that you have with the server.
-// The conn object is full of useful methods and properties that can now be used to interact with the server!
